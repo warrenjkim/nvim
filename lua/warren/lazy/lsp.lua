f2563fc0c67cc2c9ba10c65995193b5ca1f908e4
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-03-09 14:30:20 -0800

    added to lsp

--- lua/warren/lazy/lsp.lua
+++ lua/warren/lazy/lsp.lua
@@ -29,6 +29,7 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "tsserver",
+                "clangd"
             },
             handlers = {
                 function(server_name) -- default handler (optional)
@@ -66,6 +67,8 @@ return {
                 ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
                 ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
                 ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+                ['<Tab>'] = cmp.mapping.confirm({ select = true }),
+                ['<Return>'] = cmp.mapping.confirm({ select = true }),
                 ["<C-Space>"] = cmp.mapping.complete(),
             }),
             sources = cmp.config.sources({
