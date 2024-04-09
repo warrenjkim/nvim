bcd9b12b295a8884b23adf1ad5e206f0fe8f753b
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-04-08 22:23:04 -0700

    documentation and added borders around floating windows

--- lua/warren/init.lua
+++ lua/warren/init.lua
@@ -13,12 +13,15 @@ function R(name)
     require("plenary.reload").reload_module(name)
 end
 
+-- go templates
 vim.filetype.add({
     extension = {
         templ = 'templ',
     }
 })
+vim.cmd([[au BufRead,BufNewFile *.tmpl set filetype=html]])
 
+-- highlights selected text when yanked
 autocmd('TextYankPost', {
     group = yank_group,
     pattern = '*',
@@ -36,23 +39,45 @@ autocmd({"BufWritePre"}, {
     command = [[%s/\s\+$//e]],
 })
 
+-- Set up border for LSP hover
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
+  border = "single"
+})
+
+-- Set up border for LSP signature help
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+  -- Specify border style: single, double, rounded, solid, shadow
+  border = "single"
+})
+
 autocmd('LspAttach', {
     group = WarrenGroup,
     callback = function(e)
         local opts = { buffer = e.buf }
+        -- go to definition (splits window)
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+        -- floating description window
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+
+        -- find symbol in project
         vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+        -- find references of a symbol
         vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+        -- rename a symbol
         vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+        -- signature help in insert mode
         vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+
+        -- open diagnostics
+        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+        -- diagnostics scroll forward
         vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+        -- diagnostics scroll backward
         vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        -- diagnostics code action
+        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
     end
 })
 
 vim.o.background = "dark"
 vim.cmd([[colorscheme gruvbox]])
-vim.cmd([[au BufRead,BufNewFile *.tmpl set filetype=html]])
