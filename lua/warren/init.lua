4b1333f2f74d0924fca87779038b9bf92ab67d4b
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-07-16 23:06:56 -0700

    formatting

--- lua/warren/init.lua
+++ lua/warren/init.lua
@@ -10,75 +10,76 @@ local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
 
 function R(name)
-    require("plenary.reload").reload_module(name)
+  require("plenary.reload").reload_module(name)
 end
 
 -- go templates
 vim.filetype.add({
-    extension = {
-        templ = 'templ',
-    }
+  extension = {
+    templ = 'templ',
+    ddl = 'sql'
+  }
 })
 vim.cmd([[au BufRead,BufNewFile *.tmpl set filetype=html]])
 
 -- highlights selected text when yanked
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
 autocmd({ "BufWritePre" }, {
-    group = WarrenGroup,
-    pattern = "*",
-    command = [[%s/\s\+$//e]],
+  group = WarrenGroup,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
 })
 
 
 -- lsp
 autocmd('LspAttach', {
-    group = WarrenGroup,
-    callback = function(e)
-        local opts = { buffer = e.buf }
-        -- go to definition (splits window)
-        vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-        -- floating description window
-        vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  group = WarrenGroup,
+  callback = function(e)
+    local opts = { buffer = e.buf }
+    -- go to definition (splits window)
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    -- floating description window
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 
-        -- find symbol in project
-        vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-        -- find references of a symbol
-        vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-        -- rename a symbol
-        vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-        -- signature help in insert mode
-        vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    -- find symbol in project
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    -- find references of a symbol
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
+    -- rename a symbol
+    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    -- signature help in insert mode
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
 
-        -- open diagnostics
-        vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-        -- diagnostics scroll forward
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        -- diagnostics scroll backward
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-        -- diagnostics code action
-        vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    end
+    -- open diagnostics
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    -- diagnostics scroll forward
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    -- diagnostics scroll backward
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    -- diagnostics code action
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+  end
 })
 
 -- Set up border for LSP hover
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-    border = "single"
+  border = "single"
 })
 
 -- Set up border for LSP signature help
 vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-    -- Specify border style: single, double, rounded, solid, shadow
-    border = "single"
+  -- Specify border style: single, double, rounded, solid, shadow
+  border = "single"
 })
 
 
--- lua/warren/remap.lua
+++ lua/warren/remap.lua
@@ -17,10 +17,10 @@ vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
 -- copy to system clipboard
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -34,20 +34,23 @@ vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
+-- the best refactor tool known to man
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.config/nvim<CR>");
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd([[so]])
+  vim.cmd([[so]])
 end)
 
 
 -- tabs
-vim.keymap.set("n", "gt", ":tabnew<CR>", { silent = true })
-vim.keymap.set("n", "gp", ":tabnext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gt", ":tabnew<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gp", ":tabnext<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gx", ":tabclose<CR>", { silent = true })
 vim.keymap.set("n", "<leader>1", ":tabnext 1<CR>", { silent = true })
 vim.keymap.set("n", "<leader>2", ":tabnext 2<CR>", { silent = true })
 vim.keymap.set("n", "<leader>3", ":tabnext 3<CR>", { silent = true })
