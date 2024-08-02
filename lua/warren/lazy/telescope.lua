13824232f26d1c5adad9b871aa5fcc15c99b37b4
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-08-02 11:53:04 -0700

    add grep search mapping

--- lua/warren/lazy/telescope.lua
+++ lua/warren/lazy/telescope.lua
@@ -13,6 +13,8 @@ return {
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+    vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
+    vim.keymap.set('n', '<leader>pb', builtin.buffers, {})
 
     vim.keymap.set('n', '<leader>pws', function()
       local word = vim.fn.expand("<cword>")
