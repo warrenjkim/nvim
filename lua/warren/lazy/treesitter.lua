660bfda3553d6d3e31cf87c250c9fa0ad0a14644
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2024-08-07 10:33:57 -0700

    Add treeesitter-context

--- lua/warren/lazy/treesitter.lua
+++ lua/warren/lazy/treesitter.lua
@@ -1,43 +1,59 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  build = ':TSUpdate',
-  config = function()
-    require('nvim-treesitter.configs').setup({
-      -- A list of parser names, or 'all'
-      ensure_installed = {
-        'c',
-        'go',
-        'lua',
-        'java',
-        'bash',
-        'html',
-        'templ',
-        'python',
-        'vimdoc',
-        'dockerfile',
-      },
+  {
+    'nvim-treesitter/nvim-treesitter',
+    build = ':TSUpdate',
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        -- A list of parser names, or 'all'
+        ensure_installed = {
+          'c',
+          'go',
+          'lua',
+          'java',
+          'bash',
+          'html',
+          'templ',
+          'python',
+          'vimdoc',
+          'dockerfile',
+        },
 
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+        -- Install parsers synchronously (only applied to `ensure_installed`)
+        sync_install = false,
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+        -- Automatically install missing parsers when entering buffer
+        -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+        auto_install = true,
 
-      indent = {
-        enable = true
-      },
+        indent = {
+          enable = true
+        },
 
-      highlight = {
-        -- `false` will disable the whole extension
+        highlight = {
+          -- `false` will disable the whole extension
+          enable = true,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+      })
+    end
+  },
+  {
+    'nvim-treesitter/nvim-treesitter-context',
+    config = function()
+      require 'treesitter-context'.setup({
         enable = true,
+        multiline_threshold = 1,
+        separator = '-',
+      })
 
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-    })
-  end
+      vim.keymap.set("n", "[c", function()
+        require("treesitter-context").go_to_context(vim.v.count1)
+      end, { silent = true })
+    end
+  }
 }
