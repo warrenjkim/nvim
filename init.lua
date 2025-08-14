627cad7d76bb03126683eb6da4972c8a667d5331
Warren Kim <89424559+warrenjkim@users.noreply.github.com>
2025-08-13 23:44:03 -0700

    git plugins

--- init.lua
+++ init.lua
@@ -15,11 +15,16 @@ vim.pack.add({
   { src = "https://github.com/nvim-telescope/telescope.nvim",           { load = true } },
   { src = "https://github.com/isak102/telescope-git-file-history.nvim", { load = true } },
 
+  -- git
+  { src = "https://github.com/tpope/vim-fugitive",                      { load = true } },
+  { src = "https://github.com/lewis6991/gitsigns.nvim",                 { load = true } },
 })
 
 require("config.diagnostics")
 require("plugins.colors")
 require("plugins.lsp")
 require("plugins.telescope")
+require("plugins.fugitive")
+require("plugins.gitsigns")
 
 vim.keymap.set('n', '<leader><leader>c', require("config.utils").clean_unused_plugins)
